80d80fc454ab3508b97fd46715cd0388d0db5a69
Miguel Duarte Barroso <mdbarroso@redhat.com>
2023-12-11 16:37:07 +0100

    db: add initial data
    Signed-off-by: Miguel Duarte Barroso <mdbarroso@redhat.com>

---
+++ migrations/2023-12-11-153607_init-db/up.sql
@@ -0,0 +1,4 @@
+INSERT INTO ninja_turtles(name, email, weapon) values('leonardo', 'leo@tmnt.org', 'swords');
+INSERT INTO ninja_turtles(name, email, weapon) values('donatello', 'don@tmnt.org', 'a stick');
+INSERT INTO ninja_turtles(name, email, weapon) values('michaelangello', 'mike@tmnt.org', 'nunchuks');
+INSERT INTO ninja_turtles(name, email, weapon) values('raphael', 'raph@tmnt.org', 'twin sai');
